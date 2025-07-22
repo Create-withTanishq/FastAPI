8a19849d0e13af6b716ed235f02531c8cff6302a
Create-withTanishq <tanishqsaha2004@gmail.com>
2025-07-22 14:01:28 +0530

    use of hashing

---
+++ Blog/hashing.py
@@ -0,0 +1,8 @@
+# secure password management
+from passlib.context import CryptContext
+
+pwd_cxt = CryptContext(schemes = ["bcrypt"] , deprecated = "auto")
+
+class Hash():
+    def bcrypt(password : str):
+        return pwd_cxt.hash(password)
